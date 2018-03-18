de6c7c94db936856c1f3a9e814578a1111016512
Caelean Barnes <caeleanb@gmail.com>
2018-03-18 10:21:15 -0700

    testing tweaks

--- client/src/tests/Postings.test.js
+++ client/src/tests/Postings.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SpotifyMock from './Mock'
 import Postings from "../components/Postings";
+import { Provider } from 'react-redux';
+import configureStore from '../configureStore';
 
-it('renders and calls the mock api without failing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Postings api={new SpotifyMock()} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders and calls the api without failing', () => {
+    const div = document.createElement('div');
+    const store = configureStore();
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <Postings api={new SpotifyMock()} />
+        </Provider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
